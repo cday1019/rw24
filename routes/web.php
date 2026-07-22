aa757de12e849f632560af5171894fd76d67a448
cday1019 <chadd@thecawleyco.com>
2026-07-22 16:09:14 -0500

    "feat: redirect homepage to dashboard"

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 //Route::view('/', 'welcome')->name('home');
-Route::redirect('/', '/dashboard');
+Route::redirect('/', '/dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
