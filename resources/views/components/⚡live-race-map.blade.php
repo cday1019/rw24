437558f3b2add4ca65cfef4166e8ed417d3b630a
cday1019 <chadd@thecawleyco.com>
2026-07-24 14:29:58 -0500

    updated home base lat and lng

--- resources/views/components/⚡live-race-map.blade.php
+++ resources/views/components/⚡live-race-map.blade.php
@@ -256,7 +256,7 @@ new class extends Component
                 routePaths: @js($routePaths),
                 checkpoints: @js($checkpoints),
                 bonusCheckpoints: @entangle('openBonusCheckpoints'),
-                homeBasePos: { lat: 43.06210376784992, lng: -87.9037624396661 },
+                homeBasePos: { lat: 43.06210, lng: -87.90376 },
                 map: null,
 
                 init() {
